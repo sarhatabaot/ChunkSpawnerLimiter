959aae955a6bf723a368a89417cc8234d675de3b
Omer Oreg <omeroreg@gmail.com>
2025-04-22 21:05:55 +0300

    Prevent "duplicate" tasks for chunks
    Instead, if a task is supposed to be created for a chunk, and one is already running, mark it for recheck. At the end of the task, it will run again once.

---
+++ src/main/java/com/cyprias/chunkspawnerlimiter/inspection/entities/ChunkIdentifier.java
@@ -0,0 +1,41 @@
+package com.cyprias.chunkspawnerlimiter.inspection.entities;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class ChunkIdentifier {
+    private final UUID worldUuid;
+    private final int x;
+    private final int z;
+
+    public ChunkIdentifier(UUID worldUuid, int x, int z) {
+        this.worldUuid = worldUuid;
+        this.x = x;
+        this.z = z;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChunkIdentifier that = (ChunkIdentifier) o;
+        return x == that.x && z == that.z && worldUuid.equals(that.worldUuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(worldUuid, x, z);
+    }
+
+    public UUID getWorldUuid() {
+        return worldUuid;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getZ() {
+        return z;
+    }
+}
